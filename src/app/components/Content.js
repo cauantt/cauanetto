dbbe2501e61540053b7d7ecbd8c07c78dd6d770e
Cauã Netto <131816676+cauantt@users.noreply.github.com>
2024-06-20 09:05:39 -0300

    commit

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import Button from './Button'
-
+import Button from './lib/Button'
 import Image from 'next/image';
+import Contact from './lib/Contact';
+import logo from "@/app/assets/image.svg";
+import instagram from "@/app/assets/instagram.svg";
+import mail from "@/app/assets/mail.svg";
+import phone from "@/app/assets/phone.svg";
+import smile from "@/app/assets/smile.svg";
 
 export default function Content() {
+
+    
+
+
+
   return (
 
     /*CONTENT*/ 
-    <section className='content bg-escuro flex flex-col items-center'>
+    <section className='content bg-escuro flex justify-center items-center mb-96'>
 
     
-         <div className='default max-w-[1050px] w-full justify-between flex pl-7 pr-7  '>
+         <div className='default max-w-[1050px] w-full justify-between flex pl-7 pr-7 flex-col items-center '>
 
-            <div className='div1 flex justify-between w-full md:flex-col-reverse md:items-center md:pt-10 md:pb-14 md:gap-10'>
+            <div className='div1 flex justify-between items-center w-full md:flex-col-reverse md:items-center md:pt-10 md:pb-14 md:gap-10'>
 
                 <div className='left flex flex-col gap-7 justify-center h-full'>
 
@@ -28,10 +38,28 @@ export default function Content() {
 
                 <div className='right flex'>
 
-                    <Image src='/image.svg' alt='Imagem' width={400} height={400} />
+                    <Image src={logo} alt='Imagem' width={400} height={400} />
 
                 </div>
                 
+            </div>
+
+            <div className='div2 flex mt-20 flex-col justify-center items-center gap-10 text-center' >
+
+                  <p className='text-verde text-2xl'>Sobre mim</p>
+
+                  <p className=''>Olá! Sou o Cauã Netto, um entusiasta de Ciência da Computação, mergulhado no universo do desenvolvimento web. Atualmente, estou focado principalmente no front-end,
+                  desenvolvendo sites responsivos em <span className='text-verde'>Next.js</span> </p>
+
+                  <div className='flex gap-32'>
+                  <Contact imageUrl={smile} type="Nome" cont="Cauã Netto"/>
+                    <Contact imageUrl={mail} type="E-mail" cont="cauacerqueiranetto2022@gmail.com"/>
+                     <Contact imageUrl={instagram} type="Instagram" cont="@caua_ntt"/>
+                    <Contact imageUrl={phone} type="Telefone" cont="(64) 98123-1198"/>
+
+                  </div>
+
+
             </div>
 
 
---
+++ src/app/components/lib/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Contact({imageUrl, type, cont}) {
+  return (
+    <div className='flex flex-col gap-4 items-center pt-10'>
+         <div className='rounded-full bg-claro border border-gray-500 p-3 justify-center items-center'>
+           <Image src={imageUrl} alt='Imagem' width={18} height={50} />
+         </div>
+        <p>{type}</p>
+        <h3 className='text-sm'>{cont}</h3>
+
+        
+    </div>
+  )
+}
